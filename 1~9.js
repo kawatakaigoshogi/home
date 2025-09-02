4e27862ec2fd3d11acad748570087ad84b22f506
yuto-j621 <stnh.kobaton.888@gmail.com>
2025-09-02 16:28:11 +0900

    Update 1~9.js

--- 1~9.js
+++ 1~9.js
@@ -54,11 +54,11 @@ document.addEventListener('DOMContentLoaded', () => {
         switch (fileName) {
             case '7-1.png':
                 return '手順：<br>1. ☗3一飛成 ☖6二玉<br>2. ☗7一銀打 ☖同金<br>3. ☗4二龍 ☖6一玉<br>4. ☗5二龍';
-            case '7手詰めー2.png':
+            case '7-2.png':
                 return '手順：<br>1. ☗4二角成 ☖同玉<br>2. ☗4一金 ☖同玉<br>3. ☗4二飛 ☖同玉<br>4. ☗3二金 ☖同玉<br>5. ☗3一角';
-            case '9手詰めー1.png':
+            case '9-1.png':
                 return '手順：<br>1. ☗3四飛成 ☖同玉<br>2. ☗2四角成 ☖同玉<br>3. ☗3四金 ☖同玉<br>4. ☗2四銀 ☖同玉<br>5. ☗3三金';
-            case '9手詰めー2.png':
+            case '9-2.png':
                 return '手順：<br>1. ☗6一金 ☖同玉<br>2. ☗6二飛成 ☖同玉<br>3. ☗6三金 ☖同玉<br>4. ☗6四銀 ☖同玉<br>5. ☗6五角';
             default:
                 return '手順は準備中です。';
