9f221a9d806f9c0cdfad1f82c2aebb7bf2315f88
yuto-j621 <stnh.kobaton.888@gmail.com>
2025-09-04 16:27:04 +0900

    Create 21~.js

---
+++ 21~.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const checkAnswerButton1 = document.querySelector('.check-answer-1');
+    const closeButton1 = document.querySelector('.close-button-1');
+    const answerContainer1 = document.querySelector('.answer-container-1');
+
+    // ページ読み込み時に回答コンテナを非表示にする
+
+
+    // 回答を見るボタンをクリックしたときの処理
+    checkAnswerButton1.addEventListener('click', () => {
+        answerContainer1.style.display = 'flex';
+    });
+
+    // 閉じるボタンをクリックしたときの処理
+    closeButton1.addEventListener('click', () => {
+        answerContainer1.style.display = 'none';
+    });
+    const checkAnswerButton2 = document.querySelector('.check-answer-2');
+    const closeButton2 = document.querySelector('.close-button-2');
+    const answerContainer2 = document.querySelector('.answer-container-2');
+
+    // ページ読み込み時に回答コンテナを非表示にする
+
+    // 回答を見るボタンをクリックしたときの処理
+    checkAnswerButton2.addEventListener('click', () => {
+        answerContainer2.style.display = 'flex';
+    });
+
+    // 閉じるボタンをクリックしたときの処理
+    closeButton2.addEventListener('click', () => {
+        answerContainer2.style.display = 'none';
+    });
+    const checkAnswerButton3 = document.querySelector('.check-answer-3');
+    const closeButton3 = document.querySelector('.close-button-3');
+    const answerContainer3 = document.querySelector('.answer-container-3');
+
+    // ページ読み込み時に回答コンテナを非表示にする
+
+
+    // 回答を見るボタンをクリックしたときの処理
+    checkAnswerButton3.addEventListener('click', () => {
+        answerContainer3.style.display = 'flex';
+    });
+
+    // 閉じるボタンをクリックしたときの処理
+    closeButton3.addEventListener('click', () => {
+        answerContainer3.style.display = 'none';
+    });
+    const checkAnswerButton4 = document.querySelector('.check-answer-4');
+    const closeButton4 = document.querySelector('.close-button-4');
+    const answerContainer4 = document.querySelector('.answer-container-4');
+
+    // ページ読み込み時に回答コンテナを非表示にする
+
+
+    // 回答を見るボタンをクリックしたときの処理
+    checkAnswerButton4.addEventListener('click', () => {
+        answerContainer4.style.display = 'flex';
+    });
+
+    // 閉じるボタンをクリックしたときの処理
+    closeButton4.addEventListener('click', () => {
+        answerContainer4.style.display = 'none';
+    });
+});
