95d46793980e67e394bf2756fcfbe90e8fec284b
yuto-j621 <stnh.kobaton.888@gmail.com>
2025-09-05 10:00:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -99,3 +99,13 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+// ハンバーガーメニューの開閉機能
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const menuContainer = document.querySelector('.menu-container');
+
+    hamburgerMenu.addEventListener('click', () => {
+        hamburgerMenu.classList.toggle('open');
+        menuContainer.classList.toggle('open');
+    });
+});
